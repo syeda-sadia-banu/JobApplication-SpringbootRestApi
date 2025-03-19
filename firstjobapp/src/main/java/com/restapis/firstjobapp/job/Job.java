708eedc28bed982b14949067adf38758b91ec151
syeda-sadia-banu <sadia.banu17@gmail.com>
2025-03-19 16:19:31 +0530

    Spring Data Jpa with H2 Database

--- firstjobapp/src/main/java/com/restapis/firstjobapp/job/Job.java
+++ firstjobapp/src/main/java/com/restapis/firstjobapp/job/Job.java
@@ -1,7 +1,17 @@
 package com.restapis.firstjobapp.job;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="job_table")
 public class Job {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String description;
@@ -9,6 +19,11 @@ public class Job {
 	private String minSalary;
 	private String maxSalary;
 	private String location;
+	
+	
+	public Job() {
+		super();
+	}
 	public Job(Long id, String title, String description, String minSalary, String maxSalary,
 			String location) {
 		super();
--- firstjobapp/src/main/java/com/restapis/firstjobapp/job/impl/JobServiceImpl.java
+++ firstjobapp/src/main/java/com/restapis/firstjobapp/job/impl/JobServiceImpl.java
@@ -1,71 +1,91 @@
 package com.restapis.firstjobapp.job.impl;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.restapis.firstjobapp.job.Job;
+import com.restapis.firstjobapp.job.JobRepository;
 import com.restapis.firstjobapp.job.JobService;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class JobServiceImpl implements JobService {
 
-	private List<Job> jobs = new ArrayList<>();
-	private Long nextId = 1L;
+	// private List<Job> jobs = new ArrayList<>();
+
+	JobRepository jobRepository;
+
+	public JobServiceImpl(JobRepository jobRepository) {
+		super();
+		this.jobRepository = jobRepository;
+	}
+
+	//private Long nextId = 1L;
 
 	@Override
 	public List<Job> findAll() {
 
-		return jobs;
+		return jobRepository.findAll();
 	}
 
 	@Override
+	@Transactional
 	public void createJob(Job job) {
-		job.setId(nextId++);
+		//job.setId(nextId++);
 
-		jobs.add(job);
+		jobRepository.save(job);
 
 	}
 
 	@Override
+	@Transactional
 	public Job getJobById(Long id) {
-		for (Job job : jobs) {
-			if (job.getId().equals(id)) {
-
-				return job;
-			}
-
-		}
-		return null;
+		/*
+		 * for (Job job : job) { if (job.getId().equals(id)) {
+		 * 
+		 * return job; }
+		 * 
+		 * } return null;
+		 */
+		return jobRepository.findById(id).orElse(null);
 	}
 
 	@Override
+	@Transactional
 	public boolean deleteById(Long id) {
-		Iterator<Job> iterator = jobs.iterator();
-		while (iterator.hasNext()) {
-			Job job = iterator.next();
-			if (job.getId().equals(id)) {
-				iterator.remove();
-				return true;
-			}
+		/*
+		 * Iterator<Job> iterator = jobs.iterator(); while (iterator.hasNext()) { Job
+		 * job = iterator.next(); if (job.getId().equals(id)) { iterator.remove();
+		 * return true; } } return false;
+		 */
+		try {
+			jobRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
 		}
-		return false;
 	}
 
 	@Override
+	@Transactional
 	public boolean updateJob(Long id, Job updatedJob) {
-		for(Job job:jobs) {
-			if(job.getId().equals(id)) {
-				job.setTitle(updatedJob.getTitle());
-				job.setMinSalary(updatedJob.getMinSalary());
-				job.setMaxSalary(updatedJob.getMaxSalary());
-				job.setLocation(updatedJob.getLocation());
-				return true;
-			}
-		}
-		return false;
-	}
+		Optional<Job> jobOptional=jobRepository.findById(id);
+		  
+		if (jobOptional.isPresent()) {
+			Job job=jobOptional.get();
+		  job.setTitle(updatedJob.getTitle());
+		  job.setMinSalary(updatedJob.getMinSalary());
+		  job.setMaxSalary(updatedJob.getMaxSalary());
+		  job.setLocation(updatedJob.getLocation()); 
+		  jobRepository.save(job);
+		  return true; 
+		  }
+		
+	return false;
+
+}
 
 }
